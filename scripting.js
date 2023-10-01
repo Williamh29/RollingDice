1fc766784557b7d346921be4c9eb4d409d6d68bf
William <williamh29@nycstudents.net>
2023-10-01 14:53:43 -0400

    commit

--- scripting.js
+++ scripting.js
@@ -1,9 +1,8 @@
 let dice1 = 0;
 let dice2 = 0;
 let total = 0;
-
 function rollDice() {
-    dice1 = parseInt ((Math.random() * 6) + 1);
+    dice1 = parseInt((Math.random() * 6) + 1);
     dice2 = parseInt ((Math.random() * 6) + 1);
     update1();
     update2();
@@ -18,7 +17,6 @@ function update1() {
 function update2() {
     dice4.textContent = `Dice 2: ${dice2}`;
 }
-
 
 const rolling = document.querySelector("roll");
 const adding = document.querySelector("add");
